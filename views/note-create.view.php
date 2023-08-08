3ab5f0651896bd089354e8af003a723a427bc0ff
Ana Wada <anamitsuko@hotmail.com>
2023-08-08 11:20:50 +0100

    added form validation user side and db side

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -13,7 +13,15 @@ $heading = "Create Note";
                     <div class="col-span-full">
                         <label for="note" class="block text-sm font-medium leading-6 text-gray-900">Note</label>
                         <div class="mt-2">
-                            <textarea id="note" name="note" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                            <textarea id="note" 
+                            name="note" 
+                            rows="3" 
+                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            required><?= $_POST['note'] ?? '' ?> </textarea>
+
+                            <?php if(isset($errors['note'])) : ?>
+                                <p class="text-red-500 text-xs mt-2"><?= $errors['note'] ?></p>
+                                <?php endif; ?>
                         </div>
                     </div>
                 </div>
